25a9e9e7af2e70c11de64ce7de50240761aec719
kuked <aka.kudoken@gmail.com>
2016-08-07 11:49:08 +0900

    Initial commit

---
+++ 01/run_test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+run_test () {
+    echo testing $1.hdl
+    tools/HardwareSimulator.sh projects/01/$1.tst
+}
+
+root=$(git rev-parse --show-toplevel)
+if [ $? -ne 0 ];then
+    exit
+fi
+
+cp *.hdl $root/projects/01/
+cd $root/
+
+run_test Not
+run_test And
+run_test Or
+run_test Xor
+run_test Mux
+run_test DMux
