656be9d20f45c24b0540cbf411f6b10388fcdf7a
kuked <aka.kudoken@gmail.com>
2016-08-09 22:17:35 +0900

    Start subject 03

---
+++ 03/a/run_test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+run_test () {
+    echo testing $1.hdl
+    tools/HardwareSimulator.sh projects/03/a/$1.tst
+}
+
+root=$(git rev-parse --show-toplevel)
+if [ $? -ne 0 ];then
+    exit
+fi
+
+cp $root/03/a/*.hdl $root/projects/03/a/
+cd $root/
+
+run_test Bit
+run_test Register
