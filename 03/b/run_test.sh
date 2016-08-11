9c7c926ff9982afc4f2a37771bfc3f59b3a365d8
kuked <aka.kudoken@gmail.com>
2016-08-11 09:29:12 +0900

    Add RAM512.hdl

---
+++ 03/b/run_test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+run_test () {
+    echo testing $1.hdl
+    tools/HardwareSimulator.sh projects/03/b/$1.tst
+}
+
+root=$(git rev-parse --show-toplevel)
+if [ $? -ne 0 ];then
+    exit
+fi
+
+cp $root/03/b/*.hdl $root/projects/03/b/
+cd $root/
+
+run_test RAM512
